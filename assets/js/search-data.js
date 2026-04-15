c5fb79d8c50491ff842aea274e23b8d069adeb69
mateuszcedro <94639677+mateuszcedro@users.noreply.github.com>
2026-04-15 12:59:34 +0000

    Deploying to gh-pages from @ mateuszcedro/mateuszcedro.github.io@3da6eaf1669f739c3ae553d0f2cf288140ca2a7d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -495,13 +495,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/mateusz-cedro", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
